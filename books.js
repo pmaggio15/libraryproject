73619deb3b53336ae5700e60ed150c2bfea6e31d
Your Name <you@example.com>
2025-07-10 11:32:44 -0600

    books page

--- books.js
+++ books.js
@@ -1,11 +1,22 @@
-function renderBooks() {
+function renderBooks(filter) {
   const booksWrapper = document.querySelector('.books');
 
 
   const books = getBooks();
   
-  const booksHtml = books.map((book) => {
-    return `<div class="book">
+  if (filter === 'LOW_TO_HIGH') {
+    books.sort((a, b) => a.originalPrice - b.originalPrice);
+  }
+  else if (filter === 'HIGH_TO_LOW') {
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+  }
+  else if (filter === 'RATING') {
+    books.sort((a, b) => b.rating - a.rating);
+  }
+
+  const booksHtml = books
+    .map((book) => {
+      return `<div class="book">
     <figure class="book__img--wrapper">
       <img class="book__img" src="${book.url}" alt="">
     </figure>
@@ -13,26 +24,31 @@ function renderBooks() {
       ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
       <span>$${book.originalPrice.toFixed(2)}</span>
     </div>
   </div>`;
-      })
-      .join("");
+    })
+    .join("");
 
   booksWrapper.innerHTML = booksHtml;
 }
 
-function filterBooks(event) {
-  if (event.target.value === 'LOW_TO_HIGH') {
-    console.log('sort by low to high')
+function ratingsHTML (rating) {
+  let ratingHTML = "";
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fas fa-star"></i>\n';
   }
+  if (!Number.isInteger(rating)) {
+    ratingHTML +=  '<i class="fas fa-star-half-alt"></i>\n';
+  }
+  return ratingHTML;
+}
+
+function filterBooks(event) {
+  renderBooks(event.target.value);
 }
 
 setTimeout(() => {
